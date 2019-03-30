f7eb1da0096251d6e0cffb149a50010a2691550d
dogukanarat <aratdogukan@gmail.com>
2019-03-30 22:50:23 +0200

    finding first cell added...

--- router.py
+++ router.py
@@ -4,6 +4,7 @@ __author__ = "dogukanarat"
 import os
 import cv2 as cv
 import numpy as np
+from skimage.viewer import ImageViewer
 
 # defining file path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -12,7 +13,43 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 image = cv.imread(BASE_DIR + '/skel.jpg', cv.IMREAD_GRAYSCALE)
 [rows, cols] = image.shape
 
-#core algorithm
+normalizedImage = cv.normalize(
+    image, None, alpha=0, beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
+ret, binary = cv.threshold(image, 150, 255, cv.THRESH_BINARY)
+
+# finding the starting point
+done, i, j = False, 0, 0
+
+while( not done ):
+    for item in binary[i]:
+        if item == 255:
+            row = i
+            col = j
+            done = True
+        j += 1
+    j = 0
+    i += 1
+
+# finding the path
+next = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+prev = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+router = [[1, 2, 3], [4, 5, 6], [7, 8 ,9]]
+path = []
+
+done = 2
+while( not (done == 0) ):
+    for i in (-1,0,1):
+        for j in (-1, 0, 1):
+            if binary[row+i][col+j] == 255 and not i == 0 and not j == 0:
+                next[i+1][j+1] = 1
+                path.append(router[i+1][j+1])
+                row = row + i
+                col = col + j
+            else:
+                pass
+    done = done - 1
+
+print(path)
 
 if __name__ == "__main__":
     pass
