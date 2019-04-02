5e53eebc7704796e7849c3274d6abf247216da70
dogukanarat <aratdogukan@gmail.com>
2019-04-02 22:45:43 +0300

    finding path algorithm finished

--- router.py
+++ router.py
@@ -19,7 +19,6 @@ ret, binary = cv.threshold(image, 150, 255, cv.THRESH_BINARY)
 
 # finding the starting point
 done, i, j = False, 0, 0
-
 while( not done ):
     for item in binary[i]:
         if item == 255:
@@ -30,26 +29,29 @@ while( not done ):
     j = 0
     i += 1
 
-# finding the path
-next = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
-prev = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+# preparing the algorithm
 router = [[1, 2, 3], [4, 5, 6], [7, 8 ,9]]
 path = []
+done = max(row, col)
 
-done = 2
-while( not (done == 0) ):
+# finding the path 
+while( not done == 0 ):
     for i in (-1,0,1):
         for j in (-1, 0, 1):
-            if binary[row+i][col+j] == 255 and not i == 0 and not j == 0:
-                next[i+1][j+1] = 1
+            if((binary[row+i][col+j] == 255) and not( (i == 0) and (j == 0))):
                 path.append(router[i+1][j+1])
+                binary[row][col] = 0
                 row = row + i
                 col = col + j
             else:
                 pass
     done = done - 1
 
+# check the result
 print(path)
+
+# save the result
+np.savetxt('path.out', path, delimiter=',', fmt='%1u')
 
 if __name__ == "__main__":
     pass
