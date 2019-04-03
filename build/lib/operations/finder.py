749d0f5e05a5ed467fab43b4d4163586425929c0
dogukanarat <aratdogukan@gmail.com>
2019-04-03 12:39:00 +0300

    package files

---
+++ build/lib/operations/finder.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+__author__ = "dogukanarat"
+
+import os
+import cv2 as cv
+import numpy as np
+from skimage.morphology import skeletonize
+from skimage.filters import threshold_otsu
+from skimage.viewer import ImageViewer
+from skimage import io
+
+# split function definition
+def split_image(image, lower_treshold, upper_treshold):
+
+    comp = np.zeros_like(image)
+    for i in range(len(image)):
+        for j in range(len(image[i])):
+            if image[i][j] >= lower_treshold and image[i][j] <= upper_treshold:
+                comp[i][j] = 255
+            else:
+                comp[i][j] = 0
+    
+    return comp
+
+# path finder defition
+def find_path(image_file, target_file):
+
+    # reading image
+    image = cv.imread(image_file, cv.IMREAD_GRAYSCALE)
+    [rows, cols] = image.shape
+
+    # threshold image
+    _, binary = cv.threshold(image, 150, 255, cv.THRESH_BINARY)
+
+    # invert image
+    binary = 255 - binary
+
+    # erode, dilate image
+    dilation = cv.dilate(binary, np.ones((6, 6), np.uint8), iterations=3)
+
+    # marker labelling
+    _, markers = cv.connectedComponents(dilation)
+
+    # map component markers to hue val
+    labeling = np.uint8( 179 * markers / np.max(markers) )
+
+    # spliting image into component
+    component_1 = split_image(labeling, 10, 150)
+    component_2 = split_image(labeling, 150, 255)
+
+    # erode, dilate component
+    dilated_component_1 = cv.dilate(
+        component_1, np.ones((7, 7), np.uint8), iterations=3)
+    eroded_component_1 = cv.erode(
+        dilated_component_1, np.ones((7, 7), np.uint8), iterations=3)
+
+    # erode, dilate component
+    dilated_component_2 = cv.dilate(
+        component_2, np.ones((7, 7), np.uint8), iterations=3)
+    eroded_component_2 = cv.erode(
+        dilated_component_2, np.ones((7, 7), np.uint8), iterations=3)
+
+    # merging components
+    merged_image = eroded_component_1 + eroded_component_2
+
+    # inverting image
+    final_image = 255 - merged_image 
+
+    # skeleton algorithm
+    kernel = np.ones((1, 1), np.uint8)
+    opening = cv.morphologyEx(final_image, cv.MORPH_OPEN, kernel)
+    blur = cv.GaussianBlur(opening, (1, 1), 0)
+    _, th4 = cv.threshold(blur, 0, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    th4[th4 == 255] = 1
+    skel = skeletonize(th4)
+
+    # viewing the resultant image
+    #viewer = ImageViewer(skel)
+    #viewer.show()
+
+    # saving image as skeleton structure
+    skel = skel * 255
+    skel = skel.astype(np.uint8)
+    io.imsave(fname=target_file, arr=skel)
+
+    print('Finding path was successfully operated!')
+    return 0
+
+if __name__ == '__main__':
+    pass
---
+++ build/lib/operations/router.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+__author__ = "dogukanarat"
+
+import os
+import cv2 as cv
+import numpy as np
+from skimage.viewer import ImageViewer
+
+# router algorithm defition
+def find_route(image_file, data_file):
+
+    # reading image
+    image = cv.imread(image_file, cv.IMREAD_GRAYSCALE)
+    [rows, cols] = image.shape
+
+    # normalizing image
+    normalizedImage = cv.normalize(
+        image, None, alpha=0, beta=1, norm_type=cv.NORM_MINMAX, dtype=cv.CV_32F)
+    ret, binary = cv.threshold(image, 150, 255, cv.THRESH_BINARY)
+
+    # finding the starting point
+    done, i, j = False, 0, 0
+    while( not done ):
+        for item in binary[i]:
+            if item == 255:
+                row = i
+                col = j
+                done = True
+            j += 1
+        j = 0
+        i += 1
+
+    # preparing the algorithm
+    router = [[1, 2, 3], [4, 5, 6], [7, 8 ,9]]
+    path = []
+    done = max(row, col)
+
+    # finding the path 
+    while( not done == 0 ):
+        for i in (-1,0,1):
+            for j in (-1, 0, 1):
+                if((binary[row+i][col+j] == 255) and not( (i == 0) and (j == 0))):
+                    path.append(router[i+1][j+1])
+                    binary[row][col] = 0
+                    row = row + i
+                    col = col + j
+                else:
+                    pass
+        done = done - 1
+
+    # check the result
+    #print(path)
+
+    # save the result
+    np.savetxt(data_file, path, delimiter=',', fmt='%1u')
+
+    print('Creating route file was successfully done!')
+
+if __name__ == "__main__":
+    pass
