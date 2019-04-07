83cca2d64ac6654967c8f16b147b47567b6dbdd6
dogukanarat <aratdogukan@gmail.com>
2019-04-07 17:01:31 +0300

    simplified

--- main.py
+++ main.py
@@ -4,10 +4,15 @@ __author__ = "dogukanarat"
 import os
 import operations.finder
 import operations.router
+import warnings
+
+warnings.filterwarnings("ignore")
 
 # defining file path
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
-    operations.finder.find_path(BASE_DIR + '/contents/no1.png', BASE_DIR + '/skel.jpg')
-    operations.router.find_route(BASE_DIR + '/skel.jpg', BASE_DIR + '/path.out')
+    operations.finder.find_path(
+        BASE_DIR + '/contents/no1.png', BASE_DIR + '/skeleton.jpg')
+    operations.router.find_route(
+        BASE_DIR + '/skeleton.jpg', BASE_DIR + '/route.out')
