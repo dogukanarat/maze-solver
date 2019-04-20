737e860f65a49345b581f5c05b1ba46a0b122af6
dogukanarat <aratdogukan@gmail.com>
2019-04-20 17:19:58 +0300

    results added

--- src/main.py
+++ src/main.py
@@ -13,6 +13,6 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__)) # defining base dire
 
 if __name__ == '__main__':
 
-    mz = mazeSolver(BASE_DIR + '/contents/no1.png', BASE_DIR + '/results') # call the object defining the maze image
+    mz = mazeSolver(BASE_DIR + '/contents/no1.png', BASE_DIR + '/results', "RESULTFILE") # call the object defining the maze image
     mz.findPath() # finding the path ino the maze image
     mz.findRoute() # finding the route in the maze image
--- src/mazeSolver/solver.py
+++ src/mazeSolver/solver.py
@@ -7,12 +7,17 @@ from mazeSolver.router import find_route
 
 class mazeSolver(object):
     
-    def __init__(self, image, TARGET_DIR):
+    def __init__(self, image, TARGET_DIR, TARGET_NAME = False):
         self.uniqueKey = uuid.uuid4().hex[:8].upper()
         self.image = image
-        self.fileSkel = TARGET_DIR + '/' + self.uniqueKey + '-SKEL.jpg'
-        self.fileRoute  = TARGET_DIR + '/' + self.uniqueKey + '.csv'
-        self.fileCheck = TARGET_DIR + '/' + self.uniqueKey + '-CHECK' + '.jpg'
+        self.keyname = ""
+
+        if not TARGET_NAME:
+            TARGET_NAME = self.uniqueKey
+        
+        self.fileSkel = TARGET_DIR + '/' + TARGET_NAME + '-SKEL.jpg'
+        self.fileRoute = TARGET_DIR + '/' + TARGET_NAME + '.csv'
+        self.fileCheck = TARGET_DIR + '/' + TARGET_NAME + '-CHECK' + '.jpg'
 
     findPath = find_path
     findRoute = find_route
