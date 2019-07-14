rotation algorithm fixed

src/modules/Finder.py
@@ -8,6 +8,7 @@ from skimage.filters import threshold_otsu
 from skimage.viewer import ImageViewer
 from skimage import io
 
+
 # split function definition
 
 
@@ -56,7 +57,7 @@ def FindPath(self):
 
     # spliting image into component
     componentOne = self.SplitImage(labeling, 10, 150)
-    ComponentTwo = self.SplitImage(labeling, 150, 255)
+    componentTwo = self.SplitImage(labeling, 150, 255)
 
     # eroding, dilating component
     dilatedComponentOne = cv.dilate(
@@ -66,7 +67,7 @@ def FindPath(self):
 
     # eroding, dilating component
     dilatedComponentTwo = cv.dilate(
-        ComponentTwo, np.ones((7, 7), np.uint8), iterations=3)
+        componentTwo, np.ones((7, 7), np.uint8), iterations=3)
     erodedComponentTwo = cv.erode(
         dilatedComponentTwo, np.ones((7, 7), np.uint8), iterations=3)
 
@@ -90,9 +91,10 @@ def FindPath(self):
     skeletoneImage = skeletoneImage.astype(np.uint8)
     io.imsave(fname=self.fileSkeletone, arr=skeletoneImage)
 
+    self.level = skeletoneImage
     print('Finding path was successfully operated!')
 
-    return 0
+    return None
 
 
 if __name__ == '__main__':

src/modules/Router.py
@@ -10,6 +10,7 @@ from skimage import io
 
 # router algorithm defition
 
+
 def FindRoute(self):
     '''
     Definition: This module process for finding route into skeletonized image\n
@@ -28,7 +29,7 @@ def FindRoute(self):
 
     # finding the starting point
     done, i, j = False, 0, 0
-    while( not done ):
+    while(not done):
         for item in binary[i]:
             if item == 255:
                 row = i
@@ -39,32 +40,32 @@ def FindRoute(self):
         i += 1
 
     # preparing the algorithm
-    router = [[1, 2, 3], [4, 5, 6], [7, 8 ,9]]
+    router = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     path = []
     done = False
     rowTemp, colTemp = 0, 0
 
-    # finding the path 
-    while( not done ):
+    # finding the path
+    while(not done):
         rowTemp = row
         colTemp = col
-        for i in (-1,0,1):
+        for i in (-1, 0, 1):
             for j in (-1, 0, 1):
-                if((binary[row+i][col+j] == 255) and not( (i == 0) and (j == 0))):
+                if((binary[row+i][col+j] == 255) and not((i == 0) and (j == 0))):
                     path.append(router[i+1][j+1])
 
-                    #test image
+                    # test image
                     blankImage[row, col] = (0, 0, 255)
 
                     binary[row][col] = 0
                     row = row + i
                     col = col + j
-                    
+
                     #print("Status: [{},{}] in [{}, {}] was processed!".format(row, col, rows, cols), end="\r", flush=True)
                 else:
                     pass
-        if( rowTemp == row and colTemp == col ): done = True
-    #print("")
+        if(rowTemp == row and colTemp == col):
+            done = True
 
     # saving the result
     np.savetxt(self.fileRoute, path, delimiter=',', fmt='%1u')
@@ -73,9 +74,11 @@ def FindRoute(self):
     blankImage = blankImage.astype(np.uint8)
     io.imsave(fname=self.fileCheck, arr=blankImage)
 
+    self.level = blankImage
     print('Creating route file was successfully done!')
 
-    return 0
+    return None
+
 
 if __name__ == "__main__":
     pass

src/modules/Transformer.py
@@ -3,6 +3,7 @@
 import cv2 as cv
 import numpy as np
 from skimage import io
+from functools import reduce
 
 
 def FourPointTransformation(self):
@@ -43,10 +44,39 @@ def FourPointTransformation(self):
     rotatedImage = cv.warpAffine(
         image, rotationMatrix, (width, height), borderMode=cv.BORDER_REFLECT)
 
+    grayScale = cv.cvtColor(rotatedImage, cv.COLOR_BGR2GRAY)
+    grayScaleTranspose = np.transpose(grayScale)
+
+    rowIndex = 0
+    columnIndex = 0
+
+    for item in grayScale:
+        sumOfArray = sum(item)
+        averageOfArray = sumOfArray / len(item)
+        if averageOfArray <= 150:
+            topTrim = rowIndex
+            break
+        rowIndex += 1
+
+    for item in grayScaleTranspose:
+        sumOfArray = sum(item)
+        averageOfArray = sumOfArray / len(item)
+        if averageOfArray <= 150:
+            leftTrim = columnIndex
+            break
+        columnIndex += 1
+
+    croppedImage = rotatedImage[topTrim:(
+        height-topTrim), leftTrim:(width-leftTrim)]
+
+    rotatedImage = croppedImage
+
     rotatedImage = rotatedImage.astype(np.uint8)
     io.imsave(fname=self.fileRotated, arr=rotatedImage)
-
+    self.level = rotatedImage
     print('Rotation image was successfully operated!')
+
+    return None
 
 
 if __name__ == "__main__":